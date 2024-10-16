029d48eee7f550fcbbe3015aca9b496bb5a50ba2
Harsh <namastecoding@gmail.com>
2024-10-16 14:52:27 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({path: '../.env'});
+
+connectDB();
+
+/* (async () => {
+  try {
+    await mongoose.connect(`${"mongodb+srv://namastecoding:mongodbwithnextjs@lessons.ckchu.mongodb.net"}/${DB_NAME}`);
+
+    app.on("error", error =>
+      console.log("Error while connecting to DB", error),
+    );
+
+    app.listen(process.env.PORT, () => console.log("Server is listening..."));
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+})(); */
