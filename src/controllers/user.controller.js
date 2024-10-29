d9bca341f4454140c707e4db2fdb7a7084f066c3
Harsh <namastecoding@gmail.com>
2024-10-29 16:09:06 +0530

    add watch history aggregation and all user endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import jwt from "jsonwebtoken";
 import { cookieOptions } from "../constants.js";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async userId => {
   try {
@@ -266,7 +267,7 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
 });
 
 const getUserChannelProfile = asyncHandler(async (req, res) => {
-  const { username } = req.body;
+  const { username } = req.params;
 
   if (!username?.trim()) throw new ApiError(404, "Username not found!");
 
@@ -328,6 +329,58 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     );
 });
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    { $match: { _id: new mongoose.Types.ObjectId(req.user._id) } },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  if (!user?.length) throw new ApiError(404, "User does not exist!");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch history fetched successfully",
+      ),
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -339,4 +392,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory,
 };
